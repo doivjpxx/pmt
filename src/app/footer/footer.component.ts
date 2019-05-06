286e469e8869ca4b5f9dbfb8ec68bb69cff61e53
zzphoghy <doivjpxx@gmail.com>
2019-05-06 23:13:41 +0700

    a

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,67 +1,65 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { ServiceService } from '../services/service.service';
-import { FormGroup, Validators } from '@angular/forms';
-import { FormBuilder } from '@angular/forms';
-import { Router } from '@angular/router';
-
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+import { ServiceService } from "../services/service.service";
+import { FormGroup, Validators } from "@angular/forms";
+import { FormBuilder } from "@angular/forms";
+import { Router } from "@angular/router";
+import { forkJoin } from "rxjs";
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  selector: "app-footer",
+  templateUrl: "./footer.component.html",
+  styleUrls: ["./footer.component.scss"]
 })
 export class FooterComponent implements OnInit {
-  @ViewChild('imageInput') imageInput: ElementRef;
+  @ViewChild("imageInput") imageInput: ElementRef;
   form: FormGroup;
   url: any;
 
   selectedFile: File = null;
 
-  constructor(private service: ServiceService, private fb: FormBuilder, private router: Router) { }
+  constructor(
+    private service: ServiceService,
+    private fb: FormBuilder,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.form = this.fb.group({
-      name: ['', Validators.required],
-      phone: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      note: ['', Validators.required]
-    })
+      name: ["", Validators.required],
+      phone: ["", Validators.required],
+      email: ["", [Validators.required, Validators.email]],
+      note: ["", Validators.required]
+    });
   }
 
   onUploadImage(event): void {
     this.selectedFile = event.target.files[0];
     const reader: FileReader = new FileReader();
-      reader.readAsDataURL(event.target.files[0]);
-      reader.onload = () => {
-        this.url = reader.result;
-      };
+    reader.readAsDataURL(event.target.files[0]);
+    reader.onload = () => {
+      this.url = reader.result;
+    };
   }
 
-  onSubmit(){
-    this.service.uploadImage(this.selectedFile).subscribe(res=>{
+  onSubmit() {
+    forkJoin(
+      this.service.uploadImage(this.selectedFile),
+      this.service.post(this.form.value),
+    ).subscribe(res => {
       console.log(res);
+      alert('Đăng tải thành công!');
+      this.router.navigate(["/home"])
     });
-    setTimeout(()=>{
-      this.service.post(this.form.value).subscribe(res=>{
-        alert('Thành công!');
-        console.log(res);
-        this.router.navigate(["/home"])
-      });
-    }, 2000);
-    // setTimeout(()=>{
-    //   window.location.reload();
-    // },2500);
   }
 
   onBrowser(event: any): void {
     event.preventDefault();
-    const e: HTMLElement = document.getElementById('imgupload') as HTMLElement;
+    const e: HTMLElement = document.getElementById("imgupload") as HTMLElement;
     e.click();
   }
 
   onRemove(): void {
-    this.url = '';
-    this.imageInput.nativeElement.value = '';
+    this.url = "";
+    this.imageInput.nativeElement.value = "";
   }
-
 }
